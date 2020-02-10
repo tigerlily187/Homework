d480a9a3089c0851bd0577eb975ab40267d42f24
Ashley E. Whitehouse <a.whitehouse@callcpc.com>
2020-02-09 18:49:16 -0600

    Monday Feb 10

---
+++ index.js
@@ -0,0 +1,22 @@
+const pet = {
+  name: "Invidia",
+  age: 7,
+  type: {
+    dog: true,
+    size: small,
+    loud: true,
+  }
+  breed: "rottweiler",
+  food: "chicken",
+  game: "tug of war",
+  colors: "black and tan",
+  siblings: 0,
+  barks: true,
+  male: false
+};
+let me = "Ashley";
+pet.type = "cat";
+console.log = object[type]
+
+//I was a little confused at the end with the mutating
+//were we suppose to make sure that the code ran correctly?
